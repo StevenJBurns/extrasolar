075915bee3f75b57a044d7d2376193555dd29b87
Steven J Burns <stevenjburns@msn.com>
2018-06-21 16:03:38 -0600

    Changed app name in package.josn; some comments in canvas component -- nothing big; planning on adding redux next

--- src/components/IntroCanvas.js
+++ src/components/IntroCanvas.js
@@ -17,7 +17,9 @@ class CanvasHome extends Component {
   componentDidMount() {
     this.ctx.fillRect(0, 0, 1080, 600);
 
+    //build a starfield for the canvas
     for (let i = 0; i < 512; i++) {
+      // An array for aech star [x, y, o] -- x and y are randomized coords based on canvas size -- o is a random opacity from 0 to 1
       let s = [Math.floor(Math.random() * this.ctx.canvas.width), Math.floor(Math.random() * this.ctx.canvas.height), Math.random()];
       this.starfield.push(s);
     }
