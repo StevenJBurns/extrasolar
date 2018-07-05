9d6c8cf26585fbd571de6d92c4a7ccdba98da07c
StevenJBurns <stevenjburns@msn.com>
2018-07-04 23:28:38 -0600

    More AppFooter cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ class App extends Component {
         </Switch>
 
         <footer id="app-footer">
-          <h4 id="footer-h4">Steven J Burns &#8226; Galvanize Boulder &#8226; WDI g62</h4>
+          {/* <h4 id="footer-h4">Steven J Burns &#8226; Galvanize Boulder &#8226; WDI g62</h4> */}
           <h4>&copy;2018 &bull; Steven J Burns</h4>
         </footer>
       </div>
