c0ee15b42701eb8c7129f64a2375eb8f0d40e1b5
StevenJBurns <stevenjburns@msn.com>
2018-08-03 23:56:03 -0600

    Tiny syntax change in redux actions index.js

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,9 @@
 export const toggleAudioMute = audioState => ({
     type: "TOGGLE_AUDIO",
     payload: audioState
-})
+});
 
 export const changeAudioSource = audioSource => ({
     type: "CHANGE_AUDIO_SOURCE",
     payload: audioSource
-})
+});
