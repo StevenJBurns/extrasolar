4f26f71bc335366ed1c49fafe1c2a91ac316b115
StevenJBurns <stevenjburns@msn.com>
2018-08-19 15:50:41 -0600

    Added background image to PageAbout

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -16,7 +16,7 @@ export const AppHeader = (props) => {
       <img id="app-logo" src={ AppLogo } alt="ExtraSolar Logo" />
       <div id="divAppTaglines">
         <h1 id="h1-header">ExtraSolar</h1>
-        <h3 id="h2-header">Visualization of exoplanets in distant solar systems</h3>
+        <h4 id="h2-header">Visualization of exoplanets in distant solar systems</h4>
       </div>
       <AudioButton isAudioMuted={ isAudioMuted } audioSource={ currentAudioSource } />
     </header>
--- src/containers/PageAbout.jsx
+++ src/containers/PageAbout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../styles/PageAbout.css"
+
 class PageAbout extends React.Component {
   render() {
     return (
