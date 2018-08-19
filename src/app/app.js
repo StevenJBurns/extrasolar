dfb2cc77e3d6c0c7c194b5d3346ef30ae59a4aa8
StevenJBurns <stevenjburns@msn.com>
2018-08-18 19:21:59 -0600

    accidental re-build while working on Stagecoach project

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route, NavLink } from 'react-router-dom';
+
+export class App extends React.Component {
+
+  render() {
+    <div>
+      
+    </div>
+  }
+}
