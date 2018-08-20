b303fc46c5a3cf6d168c3052f2d3e2801b1fbe7e
StevenJBurns <stevenjburns@msn.com>
2018-08-20 00:23:34 -0600

    Even more style and content tweaks for PageHome and PageAbout which are nearly complete

--- src/containers/PageAbout.jsx
+++ src/containers/PageAbout.jsx
@@ -10,8 +10,9 @@ export const PageAbout = (props) => {
         <h1>About</h1>
         <hr></hr>
         <p>ExtraSolar has evolved as I've learned more about software development. The original version is <a href="http://stevenjburns-galvanize-q1-project.surge.sh">here</a>, my first major project as a student at <a href="https://www.galvanize.com/boulder">Galvanize</a> in Boulder, Colorado.</p>
-        <p>Originally a collection of chained HTML5 pages with a splash of jQuery and CSS3, I was proud of getting it to work but not satisifed overall.</p>
+        <p>Originally a collection of chained HTML5 pages with a splash of jQuery and CSS3, I was very proud of just getting it to work, but I knew I could make it better as I learned more.</p>
         <p>While learning <a href="https://reactjs.org/">React</a>, I knew immediately the modern SPA approach would achieve the native-like look, feel and performance I envisioned. Toss in Redux to handle the volume of data, D3js for a simple layer of data presentation</p>
+        <p>The only future feature I would still like to add is 3D rendering and user interaction most likely via <a href="https://threejs.org/">ThreeJS</a>.</p>
         <p>The current source code is <a href="https://github.com/StevenJBurns/extrasolar">here</a> and the live version is hosted straight from Github Pages.</p>
       </div>
     </main>
--- src/containers/PageHome.jsx
+++ src/containers/PageHome.jsx
@@ -23,10 +23,9 @@ export class PageHome extends React.Component {
         <div id="div-content-wrapper">
           <h1>ExtraSolar</h1>
           <hr></hr>
-          <p>Since 1992, over 3,500 <a href="https://en.wikipedia.org/wiki/Exoplanet">extrasolar planets</a> have been observed through various means. Less than two dozen have been <a href="https://en.wikipedia.org/wiki/List_of_directly_imaged_exoplanets">directly imaged</a> but several thousand more have been catalogued by non-visual means and data.</p>
-          <p>The extrasolar data in this project comes directly from NASA and IPAC at Caltech.  </p>
-          <p>A majority of the data comes from the <a href="https://en.wikipedia.org/wiki/Kepler_(spacecraft)">Kepler and K2</a> missions since the Kepler Space Observatory was launched in 2009. NASA maintains a list of over 5,000 potential stellar candidates for future observations.</p>
-          <p>Scientists estimate there are roughly 100 billion exoplanets in our galaxy alone. Further extrapolation predicts there are 10 billion earth size planets and of those 1 billion may be in its host star's <a href="https://en.wikipedia.org/wiki/Circumstellar_habitable_zone">habitable zone</a></p>
+          <p>Since 1992, over 3,500 <a href="https://en.wikipedia.org/wiki/Exoplanet">extrasolar planets</a> have been observed through various means. Less than two dozen have been <a href="https://en.wikipedia.org/wiki/List_of_directly_imaged_exoplanets">directly imaged</a> but several thousand more have been catalogued by non-visual means. If complete, that data lends itself to creating an abstract picture of what these distant solar systems look like.</p>
+          <p>The data in this project comes directly from NASA and IPAC at Caltech, the majority of which was collected by the <a href="https://en.wikipedia.org/wiki/Kepler_(spacecraft)">Kepler and K2</a> missions since being launched in 2009. NASA maintains a list of over 5,000 potential stellar candidates for future observations.</p>
+          <p>Scientists estimate there are roughly 100 billion exoplanets in our galaxy alone. Further extrapolation predicts there are 10 billion earth size planets, and of those 1 billion may lie in its host star's <a href="https://en.wikipedia.org/wiki/Circumstellar_habitable_zone">habitable zone</a>.</p>
           <p>For now, come browse my project to see visual plots of the solar systems with valid data... </p>
         </div>
         {/* <CanvasComponent /> */}
