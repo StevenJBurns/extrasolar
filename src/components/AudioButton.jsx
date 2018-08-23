b43b4ededab75ca42b9c94956037f8af05fe2b73
StevenJBurns <stevenjburns@msn.com>
2018-08-22 23:54:58 -0600

    Removed all references to prop-types pacjkage but is still installed in the project in node_modules

--- src/components/AudioButton.jsx
+++ src/components/AudioButton.jsx
@@ -1,6 +1,5 @@
 /* React and Redux imports */
 import React from "react";
-// import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { toggleAudioMute } from "../redux/actions/index.js";
 
