2583b0a5886c2cd3b369f38553fe5e7250c21f43
StevenJBurns <stevenjburns@msn.com>
2018-08-28 22:36:15 -0600

    Refactored PageSolarSystems to a functional component again... audio now loads correctly on all pages and persisted audio mute state works correctly from localStorage

--- src/components/pages/PageSolarSystems.jsx
+++ src/components/pages/PageSolarSystems.jsx
@@ -14,73 +14,49 @@ import "react-input-range/lib/css/index.css";
 import AudioSolarSystems from '../../assets/audio/solarsystems.ogg';
 
 
-class PageSolarSystems extends React.Component {
-  componentDidMount() {
-    changeAudioSource(AudioSolarSystems);
-  };
+export const PageSolarSystems = ({changeSource}) => {
+  changeSource(AudioSolarSystems);
 
-  componentDidUpdate() {
-    let starRadiusMinMax = this.props.stars.sort((a, b) => a -b);    
-  };
+  const style_h5 = { margin: 24 + 'px ' + 0}
 
-  filterStarsByMass(val) {
-    let filterMass = this.props.filters.starMass;
-    let filteredstars = this.props.stars.filter(star => star.st_mass <= val.max && star.st_mass >= val.min);
-  };
-
-  filterPlanetsByEcc(val) {
-
-  };
-
-  handleSliderChange(val) {
-
-  };
-
-  render() {
-    const { stars, planets } = this.props;
-
-    const style_h5 = { margin: 24 + 'px ' + 0}
-
-    return (
-      <main id="main-solarsystems" style={{color: '#DFDFDF'}}>
-        <h1>Solar Systems</h1>
-        <div id="div-filters" style={{ display: 'flex', background: '#7F7F7F', padding: 24 + 'px'}}>
-          <div id="div-filter-stars" style={{ minWidth: 288 + 'px', margin: "0 auto"}}>
-            <h5 style={style_h5}>Stellar Mass</h5>
-            {/* <InputRange draggableTrack minValue={0} maxValue={25} step={0.001}
-              value={this.props.starMass}
-              onChange={this.handleSliderChange}
-              onChangeComplete={value => this.filterStarsByMass(value)} /> */}
-            <h5 style={style_h5}>Stellar Radius</h5>
-            {/* <InputRange /> */}
-            <h5 style={style_h5}>Stellar Temperature</h5>
-            {/* <InputRange /> */}
-          </div>
-          <div id="div-filter-planets" style={{ minWidth: 288 + 'px', margin: "0 auto", display: 'flex', flexDirection: 'column'}}>
-            <h5 style={style_h5}>Planet Orbital Eccentricity</h5>
-            {/* <InputRange draggableTrack minValue={0} maxValue={1} step={0.01}
-                value={{min: 0.25, max: 0.75}}
-                onChange={ value => this.setState({sliderPlanetEccMinMax: value})}
-                onChangeComplete={value => this.filterPlanetsByEcc(value)} /> */}
-            <h5 style={style_h5}>Planetary Orbit Semi-Major Axis</h5>
-            {/* <InputRange /> */}
-            <h5 style={style_h5}>Planetary Orbit Period</h5>
-            {/* <InputRange /> */}
-          </div>
+  return (
+    <main id="main-solarsystems" style={{color: '#DFDFDF'}}>
+      <h1>Solar Systems</h1>
+      <div id="div-filters" style={{ display: 'flex', background: '#7F7F7F', padding: 24 + 'px'}}>
+        <div id="div-filter-stars" style={{ minWidth: 288 + 'px', margin: "0 auto"}}>
+          <h5 style={style_h5}>Stellar Mass</h5>
+          {/* <InputRange draggableTrack minValue={0} maxValue={25} step={0.001}
+            value={this.props.starMass}
+            onChange={this.handleSliderChange}
+            onChangeComplete={value => this.filterStarsByMass(value)} /> */}
+          <h5 style={style_h5}>Stellar Radius</h5>
+          {/* <InputRange /> */}
+          <h5 style={style_h5}>Stellar Temperature</h5>
+          {/* <InputRange /> */}
         </div>
-        <ListStars stars={stars} />
-        <ListPlanets planets={[]} />
-      </main>
-    );
-  };
+        <div id="div-filter-planets" style={{ minWidth: 288 + 'px', margin: "0 auto", display: 'flex', flexDirection: 'column'}}>
+          <h5 style={style_h5}>Planet Orbital Eccentricity</h5>
+          {/* <InputRange draggableTrack minValue={0} maxValue={1} step={0.01}
+              value={{min: 0.25, max: 0.75}}
+              onChange={ value => this.setState({sliderPlanetEccMinMax: value})}
+              onChangeComplete={value => this.filterPlanetsByEcc(value)} /> */}
+          <h5 style={style_h5}>Planetary Orbit Semi-Major Axis</h5>
+          {/* <InputRange /> */}
+          <h5 style={style_h5}>Planetary Orbit Period</h5>
+          {/* <InputRange /> */}
+        </div>
+      </div>
+      <ListStars stars={[]} />
+      <ListPlanets planets={[]} />
+    </main>
+  );
 }
 
 const mapStateToProps = state => {
-  const { lastFetch } = state.data;
   const { stars, planets } = state.data;
-  const { starMass } = state.filters;
+  // const { starMass } = state.filters;
 
-  return { lastFetch, stars, planets, starMass };
+  return { stars, planets };
 };
 
 const mapDispatchToProps = dispatch => {
