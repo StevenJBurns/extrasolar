d7b7d16e0dd2a7d533f69f2afaab2079e7dad8ab
StevenJBurns <stevenjburns@msn.com>
2018-10-15 13:49:26 -0600

    More attempts at fixing the react-virtualized list on PageSolarSystems but no luck

--- src/components/ui/StarList.jsx
+++ src/components/ui/StarList.jsx
@@ -1,15 +1,15 @@
 /* React imports */
 import React from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+// import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import { List } from "react-virtualized";
+import { AutoSizer, List } from "react-virtualized";
 
 /* Component Imports */
-import { StarItem } from "./StarItem";
+// import { StarItem } from "./StarItem";
 
 /* Assets and Style Imports */
 import "../../styles/StarList.css";
-import "../../styles/StarItem.css";
+// import "../../styles/StarItem.css";
 
 
 export const StarList = ({ stars, isLoading, error }) => {
@@ -17,22 +17,19 @@ export const StarList = ({ stars, isLoading, error }) => {
 
   if (error) return (<h2>Data Error</h2>);
   
-  const rowRenderer = ({ key, index, isScrolling, isVisible, style}) =>
-    (<div key={key} style={style}>{stars[index]["pl_hostname"]}</div>);
+  const rowRenderer = ({ key, index, isScrolling, isVisible, style}) => {
+    return (
+      <div key={key} className="div-star-item" style={null}>{stars[index]["pl_hostname"]}</div>
+    );
+  }
+
+  console.log(stars.length);
   
   return (
-      <List width={400} height={240} rowHeight={40} rowCount={stars.length} rowRenderer={rowRenderer} />
-    // <ul id="ul-star-list" style={{ backgroundColor: "#5F5F5F", margin: 64 + "px", padding: 12 + "px" }}>
-    //   <TransitionGroup className="star-list">
-    //     {
-    //       stars.map(star => (
-    //         <CSSTransition classNames="star" key={star.pl_hostname} timeout={500}>
-    //           <li className="star">{star.pl_hostname}</li>
-    //         </CSSTransition>
-    //         )
-    //       )
-    //     }
-    //   </TransitionGroup>
-    // </ul>
+    <AutoSizer>
+      {
+        width => (<List width={320} height={240} rowHeight={60} rowCount={stars.length} rowRenderer={rowRenderer} />)
+      }
+    </AutoSizer>
   );
 };
