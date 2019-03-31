a92f655721251523cf1068362a105f2381cbc41c
StevenJBurns <stevenjburns@msn.com>
2019-03-31 16:54:24 -0600

    Move connected component folder into common component folder... for now

--- src/components/common/containers/ConnectedAudioButton.js
+++ src/components/common/containers/ConnectedAudioButton.js
@@ -1,10 +1,6 @@
-/* React and Redux imports */
 import { connect } from "react-redux";
 import { toggleAudioMute } from "../redux/actions/index.js";
-
-/* Component Imports */ 
 import { AudioButton } from "../components/ui/AudioButton.jsx";
-
 
 const mapStateToProps = state => ({
   isMuted: state.ui.isAudioMuted,
