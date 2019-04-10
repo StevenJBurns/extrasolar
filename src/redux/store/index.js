f5573ea856fb48e59b16a58ea50e6d3471f5b9e8
StevenJBurns <stevenjburns@msn.com>
2019-04-09 18:29:50 -0600

    Refactor initialState for main Redux store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -6,24 +6,9 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from '../reducers';
 
-/* # of milliseconds in one hour */
-const ONE_HOUR = 60 * 60 * 1000;
-
-if (localStorage.getItem('lastFetch') === null)
-  localStorage.setItem('lastFetch', (Date.now() - ONE_HOUR).toString());
-
-if (localStorage.getItem('isAudioMuted') === null)
-  localStorage.setItem('isAudioMuted', 'false');
-
 const initialState = {
-  ui: {
-    lastDataFetch: null,
-    isAudioMuted: localStorage.getItem('isAudioMuted') === 'true' },
-    currentAudioSource: null,
-    selectedSolarSystem: null,
-  data: {
-    lastFetch: localStorage.getItem('lastFetch'),
-  },
+  ui: {},
+  data: {},
   filters: {},
   sorting: {},
 };
