0c6d4c74e16390cfe2e957f1af86aed439e97326
StevenJBurns <stevenjburns@msn.com>
2019-04-15 18:56:45 -0600

    Updates

---
+++ precache-manifest.cf4e0d4acdc2fe1419957da9384e228b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ab22153fe45e0d47be5f224703538e",
+    "url": "/extrasolar/static/media/solarsystems.11ab2215.ogg"
+  },
+  {
+    "revision": "a96cac01e57affca1a62",
+    "url": "/extrasolar/static/css/main.41e1e60b.chunk.css"
+  },
+  {
+    "revision": "27b17516034a74675040",
+    "url": "/extrasolar/static/js/runtime~main.927f8fde.js"
+  },
+  {
+    "revision": "5e39783fb3a18f273467ca2480364038",
+    "url": "/extrasolar/static/media/black-hole.5e39783f.jpg"
+  },
+  {
+    "revision": "ffc32a7e37e377adfb34",
+    "url": "/extrasolar/static/js/2.a708b10b.chunk.js"
+  },
+  {
+    "revision": "e0ee19692c51bd7e07713a82d570c099",
+    "url": "/extrasolar/static/media/home.e0ee1969.ogg"
+  },
+  {
+    "revision": "84362f9930561163df9d9dceaf61a007",
+    "url": "/extrasolar/static/media/data.84362f99.ogg"
+  },
+  {
+    "revision": "a96cac01e57affca1a62",
+    "url": "/extrasolar/static/js/main.2d244bca.chunk.js"
+  },
+  {
+    "revision": "3a62f567ef47cb0ba4b4a4e65b5e1ad0",
+    "url": "/extrasolar/static/media/about.3a62f567.ogg"
+  },
+  {
+    "revision": "c76d1ff4424f139306445351a055a72f",
+    "url": "/extrasolar/static/media/superearth.c76d1ff4.jpg"
+  },
+  {
+    "revision": "27358291fa265d207f1e2e96aa3b492a",
+    "url": "/extrasolar/static/media/beta-pictoris-b.27358291.jpg"
+  },
+  {
+    "revision": "b2eed31474fc4fcf794ec79eb715b566",
+    "url": "/extrasolar/static/media/alpha-centauri-h.b2eed314.jpg"
+  },
+  {
+    "revision": "ffc32a7e37e377adfb34",
+    "url": "/extrasolar/static/css/2.ff7040b3.chunk.css"
+  },
+  {
+    "revision": "1b2685dfea6a1a8b290561ca7e491997",
+    "url": "/extrasolar/index.html"
+  }
+];
