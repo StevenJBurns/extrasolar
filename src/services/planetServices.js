602fce9d30aebf28c776c68040e09b47b54dd73b
StevenJBurns <stevenjburns@msn.com>
2019-07-16 00:16:07 -0600

    feat: create API service files for calls to fetch NASA stars and planets

---
+++ src/services/planetServices.js
@@ -0,0 +1,1 @@
+const urlDistinctPlanets = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_letter,pl_orbeccen,pl_orbsmax,pl_orbper&format=json";
---
+++ src/services/starServices.js
@@ -0,0 +1,1 @@
+const urlDistinctStars = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=distinct%20pl_hostname,pl_cbflag,pl_pnum,st_mass,st_rad,st_teff,st_dist&order=pl_hostname&format=json";
