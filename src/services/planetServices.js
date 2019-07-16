2b0bcc0f6a538b7548717b2440c4d359f91f7dac
StevenJBurns <stevenjburns@msn.com>
2019-07-16 02:09:30 -0600

    chore: more boilerplate for starServices and planetServices files

--- src/services/planetServices.js
+++ src/services/planetServices.js
@@ -1,1 +1,5 @@
 const urlDistinctPlanets = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_letter,pl_orbeccen,pl_orbsmax,pl_orbper&format=json";
+
+export const asyncPlanetsGET = () => {
+  
+};
--- src/services/starServices.js
+++ src/services/starServices.js
@@ -1,1 +1,5 @@
 const urlDistinctStars = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=distinct%20pl_hostname,pl_cbflag,pl_pnum,st_mass,st_rad,st_teff,st_dist&order=pl_hostname&format=json";
+
+export const asyncStarsGET = () => {
+  
+};
