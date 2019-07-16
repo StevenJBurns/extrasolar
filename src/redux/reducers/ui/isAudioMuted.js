f22bae7ce497d575219f75cd1a481e84e9b1b6d0
StevenJBurns <stevenjburns@msn.com>
2019-07-16 02:07:42 -0600

    chore: remove old console logs in isAudioMuted reducer

--- src/redux/reducers/ui/isAudioMuted.js
+++ src/redux/reducers/ui/isAudioMuted.js
@@ -3,9 +3,7 @@ import { actionTypes } from "../../actionTypes";
 const initialState = JSON.parse(localStorage.getItem("isAudioMuted")) || true;
 
 export const isAudioMuted = (state = initialState, action) => {
-  // console.log('state: ', state);
   if (action.type === actionTypes.ui.AUDIO_MUTE_TOGGLE) {
-    // console.log('changing isAudioMuted', action.payload);
     let isMuted = action.payload
     localStorage.setItem("isAudioMuted", JSON.stringify(isMuted));
     return isMuted;
