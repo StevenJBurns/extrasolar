21f090b0b17bcfc3982f2a19ae7cf1d0c7dc90b6
StevenJBurns <stevenjburns@msn.com>
2019-07-16 02:05:45 -0600

    chore: clean up ordering and syntax initial Redux store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,16 +1,13 @@
-import {
-  createStore,
-  applyMiddleware
-} from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from '../reducers';
+import thunk from 'redux-thunk';
 
 const initialState = {
-  ui: {},
   data: {},
   filters: {},
   sorting: {},
+  ui: {},
 };
 
 export const store = createStore(
