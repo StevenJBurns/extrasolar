d8019ae65df668d24000ab0658cbf82d307a0674
StevenJBurns <stevenjburns@msn.com>
2019-07-16 00:26:39 -0600

    chore: reorder combineReducer keys in rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
-import { ui } from "./ui";
 import { data } from "./data";
 import { filters } from "./filters/filters";
 import { sorting } from "./sorting/sorting";
+import { ui } from "./ui";
 
 export const rootReducer = combineReducers({
-  ui,
   data, 
   filters,
-  sorting
+  sorting,
+  ui,
 });
