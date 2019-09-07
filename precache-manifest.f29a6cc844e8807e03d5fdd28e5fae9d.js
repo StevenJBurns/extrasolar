295a33ae22f4ac3e2bcae5244a3da46d235dde8a
StevenJBurns <stevenjburns@msn.com>
2019-09-07 12:33:52 -0600

    Updates

---
+++ precache-manifest.f29a6cc844e8807e03d5fdd28e5fae9d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52294d722ede165fac3c00b0226a8cf4",
+    "url": "/extrasolar/index.html"
+  },
+  {
+    "revision": "72a07ea93388da3f166a",
+    "url": "/extrasolar/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "ce24e4af20dc2a5a60e9",
+    "url": "/extrasolar/static/css/main.e89d73fe.chunk.css"
+  },
+  {
+    "revision": "72a07ea93388da3f166a",
+    "url": "/extrasolar/static/js/2.7e731e43.chunk.js"
+  },
+  {
+    "revision": "ce24e4af20dc2a5a60e9",
+    "url": "/extrasolar/static/js/main.0f1e952e.chunk.js"
+  },
+  {
+    "revision": "56de405b2bba62304efa",
+    "url": "/extrasolar/static/js/runtime~main.307a732f.js"
+  },
+  {
+    "revision": "3a62f567ef47cb0ba4b4a4e65b5e1ad0",
+    "url": "/extrasolar/static/media/about.3a62f567.ogg"
+  },
+  {
+    "revision": "b2eed31474fc4fcf794ec79eb715b566",
+    "url": "/extrasolar/static/media/alpha-centauri-h.b2eed314.jpg"
+  },
+  {
+    "revision": "27358291fa265d207f1e2e96aa3b492a",
+    "url": "/extrasolar/static/media/beta-pictoris-b.27358291.jpg"
+  },
+  {
+    "revision": "5e39783fb3a18f273467ca2480364038",
+    "url": "/extrasolar/static/media/black-hole.5e39783f.jpg"
+  },
+  {
+    "revision": "84362f9930561163df9d9dceaf61a007",
+    "url": "/extrasolar/static/media/data.84362f99.ogg"
+  },
+  {
+    "revision": "e0ee19692c51bd7e07713a82d570c099",
+    "url": "/extrasolar/static/media/home.e0ee1969.ogg"
+  },
+  {
+    "revision": "11ab22153fe45e0d47be5f224703538e",
+    "url": "/extrasolar/static/media/solarsystems.11ab2215.ogg"
+  },
+  {
+    "revision": "c76d1ff4424f139306445351a055a72f",
+    "url": "/extrasolar/static/media/superearth.c76d1ff4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/extrasolar/precache-manifest.cf4e0d4acdc2fe1419957da9384e228b.js"
+  "/extrasolar/precache-manifest.f29a6cc844e8807e03d5fdd28e5fae9d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/extrasolar/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/extrasolar/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
