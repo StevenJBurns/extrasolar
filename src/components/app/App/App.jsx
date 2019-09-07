1176562a0d43dad2332515017c5f4c875c56d35a
StevenJBurns <stevenjburns@msn.com>
2019-09-07 12:27:43 -0600

    chore: add PropTypes to imports on App.jsx

--- src/components/app/App/App.jsx
+++ src/components/app/App/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.scss';
 
 export const App = props => (
-    <React.Fragment>
-      { props.children }
-    </React.Fragment>
-  );
+  <React.Fragment>
+    { props.children }
+  </React.Fragment>
+);
