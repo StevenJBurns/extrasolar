75c4465e701a8bf3640b950007363b8fc2090222
StevenJBurns <stevenjburns@msn.com>
2019-09-07 04:48:34 -0600

    feat: more tinkering with the Bookmark component temp usage on PageAbout

--- src/components/ui/Bookmarks.jsx
+++ src/components/ui/Bookmarks.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { actionTypes } from '../../redux/actionTypes';
 import './Bookmarks.scss';
 
 const dataz = [
-  { id: 1, name: 'Mercury' },
-  { id: 2, name: 'Venus' },
-  { id: 3, name: 'Earth' },
-  { id: 4, name: 'Mars' },
-  { id: 5, name: 'Jupiter' },
-  { id: 6, name: 'Saturn' },
-  { id: 7, name: 'Uranus' },
-  { id: 8, name: 'Neptune' },
-  { id: 9, name: 'Pluto' },
+  { id: 0, name: 'Mercury' },
+  { id: 1, name: 'Venus' },
+  { id: 2, name: 'Earth' },
+  { id: 3, name: 'Mars' },
+  { id: 4, name: 'Jupiter' },
+  { id: 5, name: 'Saturn' },
+  { id: 6, name: 'Uranus' },
+  { id: 7, name: 'Neptune' },
+  { id: 8, name: 'Pluto' },
 ];
 
 export const Bookmarks = props => {
   const dispatch = useDispatch();
 
+  const bookmarks = useSelector(state => state.ui.bookmarkedSolarSystems);
+
   const handleBookmarkToggle = e => {
-    console.log(e.currentTarget);
-    
     dispatch({
       type: actionTypes.ui.BOOKMARKS_TOGGLE_SOLARSYSTEM,
       payload: +e.currentTarget.id,
@@ -34,15 +34,26 @@ export const Bookmarks = props => {
     <div>
       <ul className="ul-planet-list">
         { dataz.map(planet => (
-          <li key={planet.id} id={planet.id} className="planet-item" onClick={e => handleBookmarkToggle(e)}>
+          <li
+            id={planet.id}
+            key={planet.id}
+            className="planet-item"
+            onClick={e => handleBookmarkToggle(e)}
+            >
             <span>{planet.name}</span>
             <button type="button" className="toggle-as-favorite">+</button>
-          </li>)
+          </li>
+          )
         )}
       </ul>
       <section>
         <h3>FAVORITES</h3>
-        { useSelector(state => state.ui.bookmarkedSolarSystems).map(item => (<span key={item}>{item}</span>))}
+        <div>
+          { bookmarks.map(planetId => (
+            <span key={planetId}>{dataz[planetId].name} &bull; </span>
+            )
+          )}
+        </div>
         <button type="button" onClick={resetFavorites}>Reset Favorites</button>
       </section>
     </div>
