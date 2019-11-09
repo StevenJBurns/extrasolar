a69bdc65ed56f7e7ce92093fe9a4d628d972270b
StevenJBurns <stevenjburns@msn.com>
2019-11-08 17:29:25 -0700

    test: add a basic spec file for Page404

---
+++ src/components/pages/Page404/Page404.spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { Page404 } from './Page404';
+
+describe('<Page404 />', () => {
+	afterEach(cleanup);
+
+	const setup = overrides => {
+		const props = {
+			...overrides,
+		}
+
+		const { container, debug } = render(<Page404 {...props} />)
+
+		return {
+			props,
+			debug,
+			page404: () => container.querySelector('#page-404'),
+		};
+	}
+
+  it('renders without errors', () => {
+		const { page404, debug } = setup();
+		expect(page404()).toBeInTheDocument();
+		debug();
+	})
+});
