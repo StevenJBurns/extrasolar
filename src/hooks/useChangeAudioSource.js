0c5808913f67894c9cfcdf11185b5f0445036e32
StevenJBurns <stevenjburns@msn.com>
2019-12-08 10:47:34 -0700

    feat: boilerplate for useAudioSource Hook

---
+++ src/hooks/useChangeAudioSource.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { actionTypes } from '../../../redux/actionTypes';
+
+export const useAudioSource = audioObject => {
+  const dispatch = useDispatch();
+
+};
