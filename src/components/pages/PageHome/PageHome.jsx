001c8349ed39ed2f67131a8830ca559750332241
StevenJBurns <stevenjburns@msn.com>
2019-12-08 10:57:10 -0700

    refactor: remove Page wrapper in PageHome for layout and styling

--- src/components/pages/PageHome/PageHome.jsx
+++ src/components/pages/PageHome/PageHome.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useDocumentTitle } from '../../../hooks/useDocumentTitle';
 import { Page } from '../Page/Page';
 import { actionTypes } from '../../../redux/actionTypes';
 import { AppFooter } from '../../app/AppFooter';
@@ -9,6 +10,7 @@ import HomeOGG from '../../../assets/audio/home.ogg';
 import "./PageHome.scss";
 
 export const PageHome = props => {
+  useDocumentTitle(props.title);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
@@ -16,21 +18,19 @@ export const PageHome = props => {
   }, []);
 
   return (
-    <Page {...props}>
-      <div className="page-home">
-        <header>
-          <h1>ExtraSolar</h1>
-          <h2>Exoplanet Data Visualization</h2>
-        </header>
-        <nav>
-          <ul>
-            <li><Link to="./data">DATA</Link></li>
-            <li><Link to="./systems">SOLAR SYSTEMS</Link></li>
-            <li><Link to="./about">ABOUT</Link></li>
-          </ul>
-        </nav>
-        <AppFooter />
-      </div>
-    </Page>
+    <div className="page-home">
+      <header>
+        <h1>ExtraSolar</h1>
+        <h2>Exoplanet Data Visualization</h2>
+      </header>
+      <nav>
+        <ul>
+          <li><Link to="./data">DATA</Link></li>
+          <li><Link to="./systems">SOLAR SYSTEMS</Link></li>
+          <li><Link to="./about">ABOUT</Link></li>
+        </ul>
+      </nav>
+      <AppFooter />
+    </div>
   );
 };
