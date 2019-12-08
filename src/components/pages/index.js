e7f58b99acca81cd12345cc544b354ec6ee6a503
StevenJBurns <stevenjburns@msn.com>
2019-12-08 10:47:05 -0700

    refactor: imports and exports in page level index.js

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,18 +1,9 @@
-import { Page } from './Page/Page.jsx';
-import { PageHome } from './PageHome/PageHome.jsx';
-import { PageData } from './PageData/PageData.jsx';
-import { PageSolarSystems } from './PageSystems/PageSolarSystems.jsx';
-import { PageAbout } from './PageAbout/PageAbout.jsx';
-import { Page404 } from './Page404/Page404.jsx';
+export { Page } from './Page/Page.jsx';
+export { PageHome } from './PageHome/PageHome.jsx';
+export { PageData } from './PageData/PageData.jsx';
+export { PageSolarSystems } from './PageSystems/PageSolarSystems.jsx';
+export { PageAbout } from './PageAbout/PageAbout.jsx';
+export { Page404 } from './Page404/Page404.jsx';
 
 export { PageHeader } from './PageHeader/PageHeader';
 export { PageFooter } from './PageFooter/PageFooter';
-
-export default {
-  Page,
-  PageHome,
-  PageData,
-  PageSolarSystems,
-  PageAbout,
-  Page404,
-};
