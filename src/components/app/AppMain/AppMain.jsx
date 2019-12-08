9dd926d721a47ff3be7d2aa877291f932664dd70
StevenJBurns <stevenjburns@msn.com>
2019-12-08 10:45:17 -0700

    refactor: start elimination of AppMain.jsx component

--- src/components/app/AppMain/AppMain.jsx
+++ src/components/app/AppMain/AppMain.jsx
@@ -1,46 +1,7 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { connect } from "react-redux";
-import { useDispatch, useSelector } from 'react-redux';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { services } from '../../../services';
-import Pages from '../../pages';
-import './AppMain.scss';
 
 export const AppMain = props => {
-  const { stars, planets } = services;
-
-  React.useEffect(() => {
-    Promise.all([stars.fetchAllStars, planets.fetchAllPlanets])
-      .then(results => console.log('results: ', results));
-  }, []);
-
   return (
-    <Switch>
-      <Route exact path='/'>
-        <Pages.PageHome />
-      </Route>
-      <Route exact path='/data'>
-        <Pages.PageData />
-      </Route>
-      <Route exact path='/systems'>
-        <Pages.PageSolarSystems />
-      </Route>
-      <Route exact path='/about'>
-        <Pages.PageAbout />
-      </Route>
-      <Route>
-        <Pages.Page404 />
-      </Route>
-    </Switch>
+    <h1>NOT IMPLEMENTED</h1>
   );
 };
-
-const mapStateToProps = state => ({
-  stars: state.data.stars,
-  planets: state.data.planets,
-  lastDataFetch: state.ui.lastDataFetch,
-});
-
-/* withRouter is a HoC for Router use with Redux */
-export default withRouter(connect(mapStateToProps, null)(AppMain));
