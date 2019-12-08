009a89a85bbac47666c14e0a28509558c56bf97b
StevenJBurns <stevenjburns@msn.com>
2019-12-08 10:49:01 -0700

    refactor: use MUI Container for main tag template in Page.jsx

--- src/components/pages/Page/Page.jsx
+++ src/components/pages/Page/Page.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Container from '@material-ui/core/Container'
 import { useDocumentTitle } from '../../../hooks/useDocumentTitle';
 
 export const Page = props => {  
   useDocumentTitle(props.title);
 
-  return(
-    <main>
+  return (
+    <Container component="main" maxWidth="lg" className={props.className}>
       {props.children}
-    </main>
+    </Container>
   );
 };
