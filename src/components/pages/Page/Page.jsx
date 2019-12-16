b0defa9705234b4c41f935626a4f37fa17063fe8
StevenJBurns <stevenjburns@msn.com>
2019-12-16 05:45:39 -0700

    style: add JSS to MUI Container in Page.jsx

--- src/components/pages/Page/Page.jsx
+++ src/components/pages/Page/Page.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import Container from '@material-ui/core/Container'
+import {Container, makeStyles } from '@material-ui/core/'
 import { useDocumentTitle } from '../../../hooks/useDocumentTitle';
 
+const useStyles = makeStyles({
+  container: {
+    color: '#DDD',
+    marginTop: '4rem',
+  }
+})
+
 export const Page = props => {  
+  const classes = useStyles();
   useDocumentTitle(props.title);
 
   return (
-    <Container component="main" maxWidth="lg" className={props.className}>
+    <Container component="main" maxWidth="md" className={classes.container}>
       {props.children}
     </Container>
   );
