dd4b2eb8a2b4849b1f67f51bd8fff30b95d8017a
StevenJBurns <stevenjburns@msn.com>
2019-12-17 20:34:49 -0700

    Updates

--- precache-manifest.dd8cd8bf81ced54e6106d4770434a098.js
+++ precache-manifest.dd8cd8bf81ced54e6106d4770434a098.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "52294d722ede165fac3c00b0226a8cf4",
+    "revision": "8a5714d07c4630091c541e6928df9de8",
     "url": "/extrasolar/index.html"
   },
   {
-    "revision": "72a07ea93388da3f166a",
+    "revision": "0b9d4436b8215ec6784a",
     "url": "/extrasolar/static/css/2.6d7d54d2.chunk.css"
   },
   {
-    "revision": "ce24e4af20dc2a5a60e9",
-    "url": "/extrasolar/static/css/main.e89d73fe.chunk.css"
+    "revision": "27778be5b690456bf779",
+    "url": "/extrasolar/static/css/main.bc255020.chunk.css"
   },
   {
-    "revision": "72a07ea93388da3f166a",
-    "url": "/extrasolar/static/js/2.7e731e43.chunk.js"
+    "revision": "0b9d4436b8215ec6784a",
+    "url": "/extrasolar/static/js/2.a3ac4446.chunk.js"
   },
   {
-    "revision": "ce24e4af20dc2a5a60e9",
-    "url": "/extrasolar/static/js/main.0f1e952e.chunk.js"
+    "revision": "161de8095050f61c4f5b71bddd06031a",
+    "url": "/extrasolar/static/js/2.a3ac4446.chunk.js.LICENSE"
   },
   {
-    "revision": "56de405b2bba62304efa",
-    "url": "/extrasolar/static/js/runtime~main.307a732f.js"
+    "revision": "27778be5b690456bf779",
+    "url": "/extrasolar/static/js/main.828b3f2f.chunk.js"
+  },
+  {
+    "revision": "5eda6767d6940eb012b5",
+    "url": "/extrasolar/static/js/runtime-main.9450ce01.js"
   },
   {
     "revision": "3a62f567ef47cb0ba4b4a4e65b5e1ad0",
@@ -32,8 +36,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/extrasolar/static/media/alpha-centauri-h.b2eed314.jpg"
   },
   {
-    "revision": "27358291fa265d207f1e2e96aa3b492a",
-    "url": "/extrasolar/static/media/beta-pictoris-b.27358291.jpg"
+    "revision": "853384261ce4a6639a00810564b34dc3",
+    "url": "/extrasolar/static/media/apollo.85338426.jpg"
   },
   {
     "revision": "5e39783fb3a18f273467ca2480364038",
@@ -50,9 +54,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "11ab22153fe45e0d47be5f224703538e",
     "url": "/extrasolar/static/media/solarsystems.11ab2215.ogg"
-  },
-  {
-    "revision": "c76d1ff4424f139306445351a055a72f",
-    "url": "/extrasolar/static/media/superearth.c76d1ff4.jpg"
   }
 ]);
