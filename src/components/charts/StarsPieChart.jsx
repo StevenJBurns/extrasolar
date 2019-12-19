b8bc3b2f30b13c9e45d38d3c8c0eddfcea8ee2e3
StevenJBurns <stevenjburns@msn.com>
2019-12-19 16:01:21 -0700

    chore: lightly perusing and revisiting the StarPieChart D# graph

--- src/components/charts/StarsPieChart.jsx
+++ src/components/charts/StarsPieChart.jsx
@@ -3,34 +3,39 @@ import React from "react";
 import * as D3 from "d3";
 
 
-export const StarsPieChart = (props) => {
+export const StarsPieChart = props => {
   if (!props.starData) return null;
 
-  D3.select("#div-chart-circumbinaries").selectAll("*").remove();
+  D3.select("#section-chart-circumbinaries").selectAll("*").remove();
 
   const { starData } = props;
   // const w = 320, h = 320, r = 120;
   // const margin = { top: 8, bottom: 8, left: 8, right: 8 };
 
   const dataCircumbinaries = [
-    { group: "nonBinary", count: 0 },
+    { group: "single", count: 0 },
     { group: "binary", count: 0 },
-    { group: "unknown", count: 0 }
+    { group: "undefined", count: 0 }
   ];
 
-  if (props.starData) starData.forEach(star => {
+  if (props.starData) {
+    starData.filter(star => !isNaN(star.pl_cbflag))
+    .forEach(star => {
     switch (star["pl_cbflag"]) {
       case 0:
-      dataCircumbinaries[0]["count"]++;
+        dataCircumbinaries[0]["count"]++;
         break;
       case 1:
-      dataCircumbinaries[1]["count"]++;
+        dataCircumbinaries[1]["count"]++;
         break;
       default:
         dataCircumbinaries[2]["count"]++;
         break;
     }
-  })
+  });
+}
+
+  console.table('system types: ', dataCircumbinaries);
 
   return (<div id="div-chart-circumbinaries"></div>);
 };
