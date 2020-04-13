00274b7db508a659ac7e6a6e59543e0df30dfa1c
StevenJBurns <stevenjburns@msn.com>
2020-04-12 22:42:01 -0600

    chore: use new tables in PageData

--- src/components/pages/PageData/PageData.jsx
+++ src/components/pages/PageData/PageData.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from "react-redux";
 import {
+  Grid,
   Typography,
   Divider,
   TableContainer,
@@ -21,6 +22,7 @@ import { selectCategoricalSystemSizes } from '../../../redux/selectors/selectCat
 import { selectTopLargestStars } from '../../../redux/selectors/selectTopLargestStars';
 import { Page } from '../Page/Page';
 import { BarChart}  from "../../charts/BarChart";
+import { CategoricalPlanetCountTable} from '../../tables/CategoricalPlanetCountTable';
 import { StarsPieChart } from "../../charts/StarsPieChart";
 import { TopLargestStarsTable } from '../../tables/TopLargestStarsTable';
 // import { ScatterPlotChart } from "../../ui/ScatterPlotChart";
@@ -30,7 +32,8 @@ import "./PageData.scss";
 const useStyles = makeStyles({
   tableHead: {
     backgroundColor: '#777777',
-  }, tableCell: {
+  },
+  tableCell: {
     backgroundColor: '#999999',
   },
 });
@@ -39,10 +42,11 @@ export const PageData = props => {
   const classes = useStyles();
   const dispatch = useDispatch();
 
-  const selectStars = useSelector(state => state.data.stars);
-  const selectPlanets = useSelector(state => state.data.planets);
-  const countStars = useSelector(selectTotalStarCount()) || 'No Star Data';
-  const countPlanets = useSelector(selectTotalPlanetCount()) || 'No Planet Data';
+  const lastDataFetch = useSelector(state => state.ui.lastDataFetch);
+  const selectAllStars = useSelector(state => state.data.stars);
+  const selectAllPlanets = useSelector(state => state.data.planets);
+  const countStars = useSelector(selectTotalStarCount()) || [];
+  const countPlanets = useSelector(selectTotalPlanetCount()) || [];
   const catergoricalData = useSelector(selectCategoricalSystemSizes()) || [];
   const largestStars = useSelector(selectTopLargestStars()) || [];
 
@@ -51,24 +55,29 @@ export const PageData = props => {
   }, []);
 
   React.useEffect(() => {
-    console.log('largest start:', largestStars.map(star => ({ name: star.pl_hostname, radius: star.st_rad})));
+    console.log('largest star:',
+      largestStars.map(star => (
+        {
+          name: star.pl_hostname,
+          planetCount: star.pl_num,
+          radius: star.st_rad
+        })));
   }, [largestStars])
 
   return (
     <Page {...props}>
-      { selectStars.isFetching && <LinearProgress /> }
-      <Typography variant='h4'>DATA</Typography>
-      <Divider />
+      { selectAllStars.isFetching && <LinearProgress /> }
+      <Typography variant='h5'>DATA</Typography>
       <Typography paragraph align='justify'>The available data from NASA comes from a SQL table view that combines star and planet data together. While this allows for compact and less-effort storage, the hierarchical or relationship of planets to stars is lost. Those relationships are rebuilt in JavaScript files of these web pages.</Typography>
       <Typography paragraph align='justify'>Some exoplanet data is incomplete, as is the stellar data of their host stars. The aim of this simple web site is to visually plot and animate a rough estimation of the geometry and scale of these distant star systems. Without some of the variables, a section of stars and planets can not be plotted and therefore are not included in the snapshot of data used here.</Typography>
       <Typography paragraph align='justify'>In cases where it makes sense, some data can be 'nudged' to a default value.  For example, if the oribital eccentricity of a planet is unknown, the value can be set to 0.0 (a perfect circle) for plotting purposes.  While inaccurate, that estimated orbit will be highlighted as 'estimated' when the planet's orbit is plotted on the Solar System page while still presenting an acceptable map of that solar system. </Typography>
       <Divider />
-      <Typography variant='h4'>Most Recent Data</Typography>
-      <TableContainer>
-        <Table>
+      <Typography variant='h5'>Most Recent Data</Typography>
+      <TableContainer component="section">
+        <Table size="small">
           <TableHead className={classes.tableHead}>
             <TableRow>
-              <TableCell>Last Data Fetch</TableCell>
+              <TableCell>Last Data Fetch: {new Date(lastDataFetch).toLocaleString("en-US")}</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -82,7 +91,8 @@ export const PageData = props => {
         </Table>
       </TableContainer>
       <Divider />
-      <section id="section-chart-categorical" className="section-chart-container">
+      <CategoricalPlanetCountTable />
+      {/* <section id="section-chart-categorical" className="section-chart-container">
         <table>
           <caption>CATEGORICAL PLANET COUNTS</caption>
           <thead>
@@ -103,15 +113,15 @@ export const PageData = props => {
           </tbody>
           <tfoot>
             <tr>
-              <th colSpan={2}>Total Planets: {selectPlanets.isFetching ? <CircularProgress/> : countPlanets }</th>
+              <th colSpan={2}>Total Planets: {selectAllPlanets.isFetching ? <CircularProgress/> : countPlanets }</th>
             </tr>
           </tfoot>
         </table>
         { catergoricalData.length ? <BarChart planetData={ catergoricalData } /> : null }
         <Typography paragraph align='justify'>The ability to find exoplanets is obviously very limited given current technology and the vast distances to even the closest stars. Most solar systems have only been observed to have 1 single planet as shown in the table and chart above. Systems containing 7 and 8 planets have only been discovered once. Because of the large disparity in that data the chart plots the number of exo-systems containing X planets on an exponential Y scale. Scientists estimate the average exo system should contain several planets of varying sizes but discovering them will require advances in detection technology. </Typography>
-      </section>
+      </section> */}
       <section id="section-chart-circumbinaries">
-        <StarsPieChart starData={selectStars.list || []} />
+        <StarsPieChart starData={selectAllStars.list || []} />
       </section>
       <TopLargestStarsTable data={largestStars} />
       <Typography variant='h6'>Largest Star: {largestStars.length || <CircularProgress />}</Typography>
