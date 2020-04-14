98457a7005e6720029ab88989de2806715ceabb0
StevenJBurns <stevenjburns@msn.com>
2020-04-13 22:20:52 -0600

    chore: change import in useChangeAudioSource to new absolute syntax via NODE_PATH

--- src/hooks/useChangeAudioSource.js
+++ src/hooks/useChangeAudioSource.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { actionTypes } from '../../../redux/actionTypes';
+import { actionTypes } from 'redux/actionTypes';
 
 export const useAudioSource = audioObject => {
   const dispatch = useDispatch();
