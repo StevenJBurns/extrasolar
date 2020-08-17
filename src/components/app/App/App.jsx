a968251b613b75d65bfd6bd9317fcda1a7e9c3a9
StevenJBurns <stevenjburns@msn.com>
2020-08-17 15:17:23 -0600

    refactor: change relative imports to absolute in App.jsx

--- src/components/app/App/App.jsx
+++ src/components/app/App/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-import { getStarsThunk, getPlanetsThunk } from '../../../redux/actions/data';
-import { PageHeader, PageFooter } from '../../pages/';
+import { getStarsThunk, getPlanetsThunk } from 'redux/actions/data';
+import { PageHeader, PageFooter } from 'components/pages';
 import {
   PageHome,
   PageData,
   PageSolarSystems,
   PageAbout,
   Page404
-} from '../../pages/';
-import HomeOGG from '../../../assets/audio/home.ogg';
-import DataOGG from '../../../assets/audio/data.ogg';
-import SolarSystemsOGG from '../../../assets/audio/solarsystems.ogg';
-import AboutOGG from "../../../assets/audio/about.ogg";
-import CanvasComponent from "../../canvas/CanvasComponent";
+} from 'components/pages';
+import HomeOGG from 'assets/audio/home.ogg';
+import DataOGG from 'assets/audio/data.ogg';
+import SolarSystemsOGG from 'assets/audio/solarsystems.ogg';
+import AboutOGG from "assets/audio/about.ogg";
+import CanvasComponent from "components/canvas/CanvasComponent";
 import './App.scss';
 
 export const App = () => {
@@ -48,7 +48,7 @@ export const App = () => {
       </Route>
       <Route>
         <PageHeader />
-        <Page404 title="Not Found"/>
+        <Page404 title="Page Not Found"/>
         <PageFooter />
       </Route>
     </Switch>
