f6da890fcd7bcecdf77fa93524992782b199fb01
StevenJBurns <stevenjburns@msn.com>
2020-08-17 21:42:37 -0600

    chore: use absolute import path in Page404

--- src/components/pages/Page404/Page404.jsx
+++ src/components/pages/Page404/Page404.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Page } from '../Page/Page';
-import "../../../assets/images/black-hole.jpg";
+import "assets/images/black-hole.jpg";
 import "./Page404.scss";
 
 export const Page404 = props => {
