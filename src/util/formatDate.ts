5825dd51a9e300041742f3c04455d956078a4c1c
StevenJBurns <stevenjburns@msn.com>
2020-08-30 09:01:28 -0600

    refactor: TS conversion for util formatDate

---
+++ src/util/formatDate.ts
@@ -0,0 +1,16 @@
+import { Months } from 'constants/months';
+
+export const addLeadingZero = (n: number): string => (n <= 9 ? '0' : '') + n;
+
+export const formatLastFetch = (lastFetch: string): string => {
+  const date: Date = new Date(lastFetch);
+
+  const day = addLeadingZero(date.getDate());
+  const month = Months[date.getMonth()].short;
+  const year = date.getFullYear();
+  const hour = addLeadingZero(date.getHours());
+  const min = addLeadingZero(date.getMinutes());
+  const sec = addLeadingZero(date.getSeconds());
+
+  return `${day}-${month}-${year}\u00A0\u00A0\u00A0\u00A0${hour}:${min}:${sec}`;
+};
