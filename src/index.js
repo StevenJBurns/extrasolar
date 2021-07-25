937d89484cbbd2e8425e1afd128bb003a8c1755a
StevenJBurns <stevenjburns@msn.com>
2021-07-24 20:50:52 -0600

    chore: alias imported providers for clarity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate as ReduxPersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from 'state/store';
 import { App } from 'components/app';
@@ -9,14 +9,14 @@ import './index.scss';
 
 const render = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
+    <ReduxProvider store={store}>
+      <ReduxPersistGate persistor={persistor} loading={null}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
-      </PersistGate>
-    </Provider>,
-    document.getElementById("react-root")
+      </ReduxPersistGate>
+    </ReduxProvider>,
+    document.querySelector('#react-root')
   );
 };
 
