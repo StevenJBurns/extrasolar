b94a4a0554870f0471f9445ae6d33d9b027bffbd
StevenJBurns <stevenjburns@msn.com>
2021-07-24 20:55:16 -0600

    chore: minor clean up in AudioToggleButton

--- src/components/ui/AudioToggleButton.jsx
+++ src/components/ui/AudioToggleButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { actionTypes } from '../../state/actionTypes';
 import { useSelector, useDispatch } from 'react-redux';
+import { actionTypes } from 'state/actionTypes';
 import { IconButton } from '@material-ui/core';
 import VolumeOff from '@material-ui/icons/VolumeOff';
 import VolumeUp from '@material-ui/icons/VolumeUp';
@@ -16,15 +16,9 @@ export const AudioToggleButton = () => {
     dispatch({ type: actionTypes.ui.TOGGLE_AUDIO_MUTE, payload: !isAudioMuted });
 
   const buttonStyle = {
-    // width: '48px',
-    // height: '48px',
-    // margin: '12px',
-    // outline: 'none',
-    // borderRadius: '50%',
     padding: 0,
     background: 'Gainsboro',
     color: isAudioMuted ? 'firebrick' : 'forestgreen',
-    // background: `${ isAudioMuted ? "firebrick" : "limegreen" }`
   };
   
   return (
@@ -33,16 +27,8 @@ export const AudioToggleButton = () => {
         { isAudioMuted ? <VolumeOff fontSize="large" /> : <VolumeUp fontSize="large" /> }
       </IconButton>
       <audio autoPlay loop src={audioSourceFile} muted={isAudioMuted}>
-        <span>Your browser does not support the <code>audio</code> element.</span> 
+        <span>Your browser does not support the <code>audio</code> element.</span>
       </audio>
     </div>
   );
-};
-
-AudioToggleButton.propTypes = {
-
-};
-
-AudioToggleButton.defaultProps = {
-
 };
