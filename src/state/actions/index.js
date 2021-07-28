7fe2e0d9579edb1c7f2ac2896567076c1d3d64bf
StevenJBurns <stevenjburns@msn.com>
2021-07-27 23:43:09 -0600

    refactor: remove more old Redux action references for filtering

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -1,6 +1,1 @@
-import { actionTypes } from "../actionTypes";
-
-export const changeFilters = newFilters => ({
-  type: actionTypes.filters.CHANGE_FILTERS,
-  payload: newFilters
-});
+export { actionTypes } from "../actionTypes";
