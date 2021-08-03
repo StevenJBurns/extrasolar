c66b6d1458ae21f2bff3639f4e5e16fd64450b36
StevenJBurns <stevenjburns@msn.com>
2021-08-03 00:41:56 -0600

    chore: remove unused action

--- src/state/actions/ui/index.js
+++ src/state/actions/ui/index.js
@@ -1,12 +1,11 @@
 import { toggleAudioMute } from './audioMute';
 import { changeAudioSource } from './audioSource';
 import { changeSelectedSolarSystem } from './solarSystem';
-import { getLastDataFetchDatetime, setLastDataFetchDatetime } from './lastDataFetch';
+import { setLastDataFetchDatetime } from './lastDataFetch';
 
 export {
   toggleAudioMute,
   changeAudioSource,
   changeSelectedSolarSystem,
-  getLastDataFetchDatetime,
   setLastDataFetchDatetime,
 };
