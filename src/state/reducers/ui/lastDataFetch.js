cd68ab562a9ba57706f3caee70f9664af7bb3e1b
StevenJBurns <stevenjburns@msn.com>
2021-08-03 00:54:51 -0600

    chore: clean up lastDataFetch reducer

--- src/state/reducers/ui/lastDataFetch.js
+++ src/state/reducers/ui/lastDataFetch.js
@@ -3,19 +3,10 @@ import { actionTypes } from "../../actionTypes";
 /* # of milliseconds in one hour */
 // const ONE_HOUR = 60 * 60 * 1000;
 
-// if (localStorage.getItem('lastFetch') === null)
-//   localStorage.setItem('lastFetch', (Date.now() - ONE_HOUR).toString());
-
-// const initialState = JSON.parse(localStorage.getItem("lastDataFetch")) || new Date();
-const initialState = new Date();
-
-export const lastDataFetch = (state = initialState, action) => {
+export const lastDataFetch = (state = Date.now(), action) => {
   switch (action.type) {
-    case actionTypes.ui.GET_LAST_DATA_FETCH_DATETIME:
-      return {...state, lastDataFetch: action.payload}
     case actionTypes.ui.SET_LAST_DATA_FETCH_DATETIME:
-      localStorage.setItem('lastDataFetch', action.payload);
-      return action.payload;
+      return new Date(action.payload);
     default:
       return state;
   };
