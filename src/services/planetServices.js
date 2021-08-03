a79360061b8d7cd3e4f321f70d088a1ec7b31921
StevenJBurns <stevenjburns@msn.com>
2021-08-03 00:40:31 -0600

    refactor: clean up services files; these files not yet used

--- src/services/planetServices.js
+++ src/services/planetServices.js
@@ -1,9 +1,13 @@
-const urlDistinctPlanets = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_letter,pl_orbeccen,pl_orbsmax,pl_orbper,pl_masse,pl_rade&format=json';
-const urlPlanetBySolarSystemId = ';'
+const urlDistinctPlanets = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+hostname,pl_letter,pl_orbeccen,pl_orbsmax,pl_orbper,pl_masse,pl_rade&format=json';
+const urlPlanetBySolarSystemId = '';
 const urlPlanetById = '';
 
-export const fetchAllPlanets = () => fetch(urlDistinctPlanets).then(data => data.json());
+export const fetchAllPlanets = () => fetch(urlDistinctPlanets)
+  .then(data => data.json())
+  .catch(err => console.error(err.message));
 
-export const fetchPlanetById = id => fetch(urlPlanetById).then(planet => planet.json());
+export const fetchPlanetById = id => fetch(urlPlanetById)
+  .then(planet => planet.json())
+  .catch(err => console.error(err.message));
 
 export const fetchPlanetBySolarSystemId = id => fetch(urlDistinctPlanets)
--- src/services/starServices.js
+++ src/services/starServices.js
@@ -1,6 +1,11 @@
-const urlDistinctStars = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=distinct%20pl_hostname,pl_cbflag,pl_pnum,st_mass,st_rad,st_teff,st_dist&order=pl_hostname&format=json';
+const urlDistinctStars = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+hostname,sy_snum,cb_flag,sy_pnum,st_mass,st_rad,st_teff,sy_dist+from+ps+order+by+hostname&format=json';
 const urlStarById = '';
 
-export const fetchAllStars = fetch(urlDistinctStars).then(data => data.json());
+export const fetchAllStars = fetch(urlDistinctStars)
+  .then(data => data.json())
+  .then(data => console.log({data}))
+  .catch(err => console.error(err.message));
 
-export const fetchStarById = fetch(urlStarById).then(star => star.json());
+export const fetchStarById = fetch(urlStarById)
+  .then(star => star.json())
+  .catch(err => console.error(err.message));
