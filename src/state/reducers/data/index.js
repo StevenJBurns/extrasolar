2a15c3acca246e79df7162e76ebbb05d487a66d3
StevenJBurns <stevenjburns@msn.com>
2021-08-03 00:44:46 -0600

    chore: remove unused actioTypes and unused solarSystem reducer

--- src/state/reducers/data/index.js
+++ src/state/reducers/data/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { solarSystems } from './solarSystems';
 import { stars } from './stars';
 import { planets } from './planets';
 
 export const data = combineReducers({
   stars,
   planets,
-  solarSystems,
 });
