9b9d8e0a9093cd4b2d9db2395bc99b0310290398
StevenJBurns <stevenjburns@msn.com>
2021-08-03 00:43:39 -0600

    chore: remove unused action (setLastDataFetchDatetime)

--- src/state/actions/ui/lastDataFetch.js
+++ src/state/actions/ui/lastDataFetch.js
@@ -1,10 +1,5 @@
 import { actionTypes } from '../../actionTypes';
 
-export const getLastDataFetchDatetime = () => ({
-  type: actionTypes.ui.GET_LAST_DATA_FETCH_DATETIME,
-  payload: JSON.parse(localStorage.getItem("lastDataFetch"))
-});
-
 export const setLastDataFetchDatetime = date => ({
   type: actionTypes.ui.SET_LAST_DATA_FETCH_DATETIME,
   payload: date,
