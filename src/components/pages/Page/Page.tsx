433fbfbf8996dc9ceab70003ab2399567ef92da5
StevenJBurns <stevenjburns@msn.com>
2022-10-24 13:40:36 -0600

    fix: convert Page base file to TS; clean up typings

--- src/components/pages/Page/Page.tsx
+++ src/components/pages/Page/Page.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import { Container } from '@material-ui/core/';
 import { useDocumentTitle } from 'hooks/useDocumentTitle';
 import { useStyles } from './Page.styles';
 
-export const Page = props => {
+interface IProps {
+  title: string;
+  children: Array<ReactNode>;
+}
+
+export const Page = (props: IProps): ReactNode => {
   const classes = useStyles();
   useDocumentTitle(props.title);
 
