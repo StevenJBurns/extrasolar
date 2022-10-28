a766393f67b6569c5322186134a5dee1bb6445e4
StevenJBurns <stevenjburns@msn.com>
2022-10-28 03:08:12 -0600

    refactor: convert root index file to TS; start using React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate as ReduxPersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,17 +6,18 @@ import { store, persistor } from 'state/store';
 import { App } from 'components/app';
 import './index.scss';
 
-const render = () => {
-  ReactDOM.render(
+const reactRoot = document.getElementById('react-root');
+
+if (reactRoot) {
+  const root = createRoot(reactRoot);
+
+  root.render(
     <ReduxProvider store={store}>
       <ReduxPersistGate persistor={persistor} loading={null}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </ReduxPersistGate>
-    </ReduxProvider>,
-    document.querySelector('#react-root')
+    </ReduxProvider>
   );
-};
-
-render();
+}
