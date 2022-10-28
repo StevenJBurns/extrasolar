b79c400556845cab23fdab51ea9f5fbbcc3f04dc
StevenJBurns <stevenjburns@msn.com>
2022-10-28 03:27:41 -0600

    refactor: convert Pages index to TS

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,8 +1,8 @@
-export { Page } from './Page/Page.jsx';
+export { Page } from './Page';
 export { PageHome } from './PageHome/PageHome.jsx';
 export { PageData } from './PageData/PageData.jsx';
-export { PageSolarSystems } from './PageSystems/PageSolarSystems.jsx';
-export { PageBookmarks } from './PageBookmarks/PageBookmarks.tsx';
+export { PageSystems } from './PageSystems/PageSystems';
+export { PageBookmarks } from './PageBookmarks/PageBookmarks';
 export { PageAbout } from './PageAbout/PageAbout.jsx';
 export { Page404 } from './Page404/Page404.jsx';
 
