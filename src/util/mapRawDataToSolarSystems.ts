a337e8a2d54442a8096e8b8dd5e9bef2bada0bf9
StevenJBurns <stevenjburns@msn.com>
2022-10-28 03:06:26 -0600

    chore: temp commented during transition to TS

--- src/util/mapRawDataToSolarSystems.ts
+++ src/util/mapRawDataToSolarSystems.ts
@@ -1,15 +1,17 @@
-import { Star, Planet } from 'types';
+// import { Star, Planet } from 'types';
 
-export const mapRawDataToSolarSystems = (stars: Array<Star>, planets: Array<Planet>) => {
-  return stars.map(star => {
-    let filteredPlanets = planets.filter(planet => 
-        planet.hostName === star.name
-      );
+// export const mapRawDataToSolarSystems = (stars: Array<Star>, planets: Array<Planet>) => {
+//   return stars.map(star => {
+//     let filteredPlanets = planets.filter(planet => 
+//         planet.hostName === star.name
+//       );
 
-    return {
-      id: star.name,
-      star: star,
-      planets: filteredPlanets
-    }
-  });
-};
+//     return {
+//       id: star.name,
+//       star: star,
+//       planets: filteredPlanets
+//     }
+//   });
+// };
+
+export const mapRawDataToSolarSystems = () => {};
