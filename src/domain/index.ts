d25072f6b92e6a9710cf67c329c9bbcc8f15cff8
SJB <stevenjburns@msn.com>
2025-01-24 04:54:12 -0600

    chore: fix another import in domain index.ts

---
+++ src/domain/index.ts
@@ -0,0 +1,4 @@
+export { Star } from './Star/Star.class';
+export { Planet } from './Planet/Planet.class';
+export { SolarSystem } from './SolarSystem/SolarSystem.class';
+export { Bookmark } from './Bookmark/Bookmark.class';
