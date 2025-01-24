16caffeea3fa0971f606e8ab1ed51b14141c7843
SJB <stevenjburns@msn.com>
2025-01-24 04:46:34 -0600

    test: simple rename of useDocumentTitle from .spec to .test

--- src/presentation/hooks/useDocumentTitle.spec.js
+++
@@ -1,9 +0,0 @@
-// import { render } from '@testing-library/react';
-import { useDocumentTitle } from './useDocumentTitle';
-
-describe('the useDocumentTitle custom hook', () => {
-  it('returns a basic document title for falsey input values', () => {
-    const falseyTitle = useDocumentTitle(0);
-    expect(falseyTitle).toEqual('ExtraSolar');
-  });
-});
