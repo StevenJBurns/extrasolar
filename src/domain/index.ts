5b609cd94f3bc81dbf377b1d5050d4c9a8a68f88
SJB <stevenjburns@msn.com>
2025-03-05 09:32:38 -0600

    refactor: update domain root index file imports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,4 +1,4 @@
-export { Star } from './Star/Star.class';
-export { Planet } from './Planet/Planet.class';
-export { SolarSystem } from './SolarSystem/SolarSystem.class';
-export { Bookmark } from './Bookmark/Bookmark.class';
+export { Star } from './entities/Star/Star.class';
+export { Planet } from './entities/Planet/Planet.class';
+export { SolarSystem } from './entities/SolarSystem/SolarSystem.class';
+export { Bookmark } from './entities/Bookmark/Bookmark.class';
