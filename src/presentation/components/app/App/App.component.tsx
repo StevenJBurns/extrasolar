5ca0926c96944e56685e1cad42188bed2f99918c
StevenJBurns <StevenJBurns@users.noreply.github.com>
2025-03-07 03:09:34 -0600

    refactor: clean up App component (#49)
    * style: remove unnecessary React fragment
    
    * refactor: remove old App and .scss styles
    
    * refactor: extract routes and styles to independent files; re-write App with imported pieces
    
    * refactor: remove React fragment from PageHome
    
    * refactor: rename route config stuff to validRoutes

--- src/presentation/components/app/App/App.component.tsx
+++ src/presentation/components/app/App/App.component.tsx
@@ -1,37 +1,22 @@
-import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router';
-import {
-  PageHome,
-  PageData,
-  PageSystems,
-  PageBookmarks,
-  PageAbout,
-  Page404,
-} from '../../pages';
+import { validRoutes } from '../../../config/validRoutes';
+import { Container } from '@mui/material';
+import { styles } from './App.styles';
 
 export const App = () => {
   return (
-    <>
-      <Container
-        maxWidth={false}
-        disableGutters
-        sx={{
-          flex: '1 0 auto',
-          display: 'flex',
-          minHeight: '100vh',
-          flexDirection: 'column',
-        }}
-      >
-        <Routes>
-          <Route index element={<PageHome />} />
-          <Route path="data" element={<PageData data={[]} />} />
-          <Route path="systems" element={<PageSystems />} />
-          <Route path="bookmarks" element={<PageBookmarks />} />
-          <Route path="about" element={<PageAbout />} />
-          <Route path="*" element={<Page404 title="Page Not Found" />} />
-        </Routes>
-        {/* <AppSnackbar hasErrors={hasErrors} /> */}
-      </Container>
-    </>
+    <Container sx={styles} maxWidth={false} disableGutters>
+      <Routes>
+        {validRoutes.map(route => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.element}
+            index={route.index}
+          />
+        ))}
+      </Routes>
+      {/* <AppSnackbar hasErrors={hasErrors} /> */}
+    </Container>
   );
 };
---
+++ src/presentation/config/validRoutes.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from 'react';
+import {
+  PageHome,
+  PageData,
+  PageSystems,
+  PageBookmarks,
+  PageAbout,
+  Page404,
+} from '../components/pages';
+
+type ValidRoute = {
+  path: string;
+  element: ReactElement;
+  index?: boolean;
+};
+
+export const validRoutes: Array<ValidRoute> = [
+  { path: '/', element: <PageHome />, index: true },
+  { path: 'data', element: <PageData data={[]} /> },
+  { path: 'systems', element: <PageSystems /> },
+  { path: 'bookmarks', element: <PageBookmarks /> },
+  { path: 'about', element: <PageAbout /> },
+  { path: '*', element: <Page404 title="Page Not Found" /> },
+];
