b3254cc98f5089ce36a33ea6797da704b933d767
SJB <stevenjburns@msn.com>
2025-03-07 01:48:50 -0600

    style: remove unnecessary React fragment

--- src/presentation/components/app/App/App.component.tsx
+++ src/presentation/components/app/App/App.component.tsx
@@ -11,27 +11,25 @@ import {
 
 export const App = () => {
   return (
-    <>
-      <Container
-        maxWidth={false}
-        disableGutters
-        sx={{
-          flex: '1 0 auto',
-          display: 'flex',
-          minHeight: '100vh',
-          flexDirection: 'column',
-        }}
-      >
-        <Routes>
-          <Route index element={<PageHome />} />
-          <Route path="data" element={<PageData data={[]} />} />
-          <Route path="systems" element={<PageSystems />} />
-          <Route path="bookmarks" element={<PageBookmarks />} />
-          <Route path="about" element={<PageAbout />} />
-          <Route path="*" element={<Page404 title="Page Not Found" />} />
-        </Routes>
-        {/* <AppSnackbar hasErrors={hasErrors} /> */}
-      </Container>
-    </>
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{
+        flex: '1 0 auto',
+        display: 'flex',
+        minHeight: '100vh',
+        flexDirection: 'column',
+      }}
+    >
+      <Routes>
+        <Route index element={<PageHome />} />
+        <Route path="data" element={<PageData data={[]} />} />
+        <Route path="systems" element={<PageSystems />} />
+        <Route path="bookmarks" element={<PageBookmarks />} />
+        <Route path="about" element={<PageAbout />} />
+        <Route path="*" element={<Page404 title="Page Not Found" />} />
+      </Routes>
+      {/* <AppSnackbar hasErrors={hasErrors} /> */}
+    </Container>
   );
 };
