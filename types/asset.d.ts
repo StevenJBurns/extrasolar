858d87cb92a104ec91531b074d0467074cde229a
SJB <stevenjburns@msn.com>
2025-03-07 04:17:40 -0600

    chore: fix eslint rules complaining about asset file imports

--- types/asset.d.ts
+++ types/asset.d.ts
@@ -2,3 +2,13 @@ declare module "*.ogg" {
   const value: unknown;
   export default value;
 }
+
+declare module '@assets/images/*' {
+  const src: string;
+  export default src;
+}
+
+declare module '@assets/audio/*' {
+  const src: string;
+  export default src;
+}
