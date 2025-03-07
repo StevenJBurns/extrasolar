d07caf7ed16d6d9b327d942056d9dffe6dc139e1
SJB <stevenjburns@msn.com>
2025-03-07 11:25:25 -0600

    refactor: change pages export to be a single Page object; update validRoutes

--- src/presentation/components/pages/index.ts
+++ src/presentation/components/pages/index.ts
@@ -1,10 +1,18 @@
-export { Page } from './Page';
-export { PageHome } from './PageHome/PageHome.component';
-export { PageData } from './PageData/PageData.component';
-export { PageSystems } from './PageSystems/PageSystems.component';
-export { PageBookmarks } from './PageBookmarks/PageBookmarks.component';
-export { PageAbout } from './PageAbout/PageAbout.component';
-export { Page404 } from './Page404/Page404';
-
 export { PageHeader } from './PageHeader/PageHeader';
 export { PageFooter } from './PageFooter/PageFooter.component';
+
+import { PageHome } from './PageHome/PageHome.component';
+import { PageData } from './PageData/PageData.component';
+import { PageSystems } from './PageSystems/PageSystems.component';
+import { PageBookmarks } from './PageBookmarks/PageBookmarks.component';
+import { PageAbout } from './PageAbout/PageAbout.component';
+import { NotFound } from './NotFound/NotFound';
+
+export const Pages = {
+  PageHome,
+  PageData,
+  PageSystems,
+  PageBookmarks,
+  PageAbout,
+  NotFound,
+};
--- src/presentation/config/validRoutes.tsx
+++ src/presentation/config/validRoutes.tsx
@@ -1,12 +1,5 @@
 import { ReactElement } from 'react';
-import {
-  PageHome,
-  PageData,
-  PageSystems,
-  PageBookmarks,
-  PageAbout,
-  Page404,
-} from '../components/pages';
+import { Pages } from '../components/pages';
 
 type ValidRoute = {
   path: string;
@@ -15,10 +8,10 @@ type ValidRoute = {
 };
 
 export const validRoutes: Array<ValidRoute> = [
-  { path: '/', element: <PageHome />, index: true },
-  { path: 'data', element: <PageData data={[]} /> },
-  { path: 'systems', element: <PageSystems /> },
-  { path: 'bookmarks', element: <PageBookmarks /> },
-  { path: 'about', element: <PageAbout /> },
-  { path: '*', element: <Page404 title="Page Not Found" /> },
+  { path: '/', element: <Pages.PageHome />, index: true },
+  { path: 'data', element: <Pages.PageData data={[]} /> },
+  { path: 'systems', element: <Pages.PageSystems /> },
+  { path: 'bookmarks', element: <Pages.PageBookmarks /> },
+  { path: 'about', element: <Pages.PageAbout /> },
+  { path: '*', element: <Pages.NotFound /> },
 ];
