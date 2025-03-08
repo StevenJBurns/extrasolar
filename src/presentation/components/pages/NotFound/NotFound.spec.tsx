b0399f95b64a0f23acded744c4143981eae7b4a8
SJB <stevenjburns@msn.com>
2025-03-07 21:00:45 -0600

    chore: clean up NotFound.spec.tsx

--- src/presentation/components/pages/NotFound/NotFound.spec.tsx
+++ src/presentation/components/pages/NotFound/NotFound.spec.tsx
@@ -1,27 +1,6 @@
-import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import { NotFound } from './NotFound.component';
 
 describe('<NotFound />', () => {
   afterEach(cleanup);
-
-  // const setup = overrides => {
-  //   const props = {
-  //     ...overrides,
-  //   };
-
-  //   const { container, debug } = render(<NotFound {...props} />);
-
-  //   return {
-  //     props,
-  //     debug,
-  //     page404: () => container.querySelector('#page-404'),
-  //   };
-  // };
-
-  // it('renders without errors', () => {
-  //   const { page404, debug } = setup();
-  //   expect(page404()).toBeInTheDocument();
-  //   debug();
-  // });
 });
