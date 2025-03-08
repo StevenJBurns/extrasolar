9247c34513f7054057cf9326f81fc6f304d0b491
SJB <stevenjburns@msn.com>
2025-03-08 08:19:48 -0600

    chore: clean up commented code in App.styles.tsx

--- src/presentation/components/app/App/App.styles.tsx
+++ src/presentation/components/app/App/App.styles.tsx
@@ -6,22 +6,3 @@ export const styles: SxProps<Theme> = {
   flexDirection: 'column',
   flex: '1 0 auto',
 };
-
-// import { SxProps } from '@mui/material/styles';
-
-// type Styles = {
-//   app: SxProps;
-//   page: SxProps;
-// };
-
-// export const styles: Styles = {
-//   app: {
-//     display: 'flex',
-//     minHeight: '100vh',
-//     flexDirection: 'column',
-//     textAlign: 'center',
-//   },
-//   page: {
-//     flex: '1 0 auto',
-//   },
-// };
