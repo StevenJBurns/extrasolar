03f43dbcea48957b5cbfb6b45514d6e48456e7f1
SJB <stevenjburns@msn.com>
2025-03-08 09:45:43 -0600

    refactor: use consistent file extensions in imports

--- src/presentation/components/pages/index.ts
+++ src/presentation/components/pages/index.ts
@@ -2,11 +2,11 @@ export { PageHeader } from './PageHeader/PageHeader';
 export { PageFooter } from './PageFooter/PageFooter.component';
 
 import { Home } from './Home/Home.component.tsx';
-import { Data } from './Data/Data.component';
-import { SolarSystems } from './SolarSystems/SolarSystems.component';
+import { Data } from './Data/Data.component.tsx';
+import { SolarSystems } from './SolarSystems/SolarSystems.component.tsx';
 import { Bookmarks } from './Bookmarks/Bookmarks.component.tsx';
-import { About } from './About/About.component';
-import { NotFound } from './NotFound/NotFound.component';
+import { About } from './About/About.component.tsx';
+import { NotFound } from './NotFound/NotFound.component.tsx';
 
 export const Pages = {
   Home,
