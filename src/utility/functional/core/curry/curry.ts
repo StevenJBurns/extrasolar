dc3d3a30654673a19b1a33644673203617fcf1b1
SJB <stevenjburns@msn.com>
2025-03-11 22:13:26 -0500

    chore: update Array<> syntax

--- src/utility/functional/core/curry/curry.ts
+++ src/utility/functional/core/curry/curry.ts
@@ -1,6 +1,6 @@
 export function curry<T extends any[], R>(fn: (...args: T) => R, arity = fn.length) {
-  return (...args: any[]): any => 
+  return (...args: Array<any>): any => 
     args.length >= arity 
       ? fn(...args.slice(0, arity) as T) 
-      : (...moreArgs: any[]) => curry(fn, arity)(...args, ...moreArgs);
+      : (...moreArgs: Array<any>) => curry(fn, arity)(...args, ...moreArgs);
 };
