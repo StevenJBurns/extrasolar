030abdb03d522f79f27aedf48eb776dc0cb437dd
SJB <stevenjburns@msn.com>
2025-03-14 01:51:46 -0500

    refactor: clean up things related to FNZPN

--- src/domain/primitives/FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.factory.ts
+++ src/domain/primitives/FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.factory.ts
@@ -1,7 +1,7 @@
 import { FiniteNonZeroPositiveNumber } from './FiniteNonZeroPositiveNumber.type.ts';
 import { Either, Left, Right } from '../../../utility/functional/monads/Either/Either.ts';
 
-export function createNonZeroPositiveNumber(
+export function createFiniteNonZeroPositiveNumber(
   value: number
 ): Either<Error, FiniteNonZeroPositiveNumber> {
   return value > 0
--- src/domain/primitives/FiniteZeroPositiveNumber/FiniteNonZeroPositiveNumber.error.ts
+++
@@ -1,7 +0,0 @@
-import { DomainError } from '@domain/types/errors/DomainError/DomainError.interface';
-
-export type NonZeroPositiveNumberError = DomainError & {
-  type: 'InvalidNonZeroPositiveNumber';
-  reason: 'nonPositive';
-  value: number;
-};
