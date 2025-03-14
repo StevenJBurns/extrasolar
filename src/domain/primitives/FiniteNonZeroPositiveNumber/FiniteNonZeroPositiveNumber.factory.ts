1a38ec71269632c080c10e453585256daf0b02f5
SJB <stevenjburns@msn.com>
2025-03-14 02:09:58 -0500

    test: create FNZPN passing tests with adjustments to factory

--- src/domain/primitives/FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.factory.ts
+++ src/domain/primitives/FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.factory.ts
@@ -1,10 +1,18 @@
 import { FiniteNonZeroPositiveNumber } from './FiniteNonZeroPositiveNumber.type.ts';
+import {
+  FiniteNonZeroPositiveNumberError,
+  createFiniteNonZeroPositiveNumberError
+} from '@domain/primitives/FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.error.ts';
 import { Either, Left, Right } from '../../../utility/functional/monads/Either/Either.ts';
 
 export function createFiniteNonZeroPositiveNumber(
   value: number
-): Either<Error, FiniteNonZeroPositiveNumber> {
-  return value > 0
-    ? Right(value as FiniteNonZeroPositiveNumber)
-    : Left(new Error('Value must be greater than zero'));
+): Either<FiniteNonZeroPositiveNumberError, FiniteNonZeroPositiveNumber> {
+  const isInfinite = value === Infinity;
+  const isNaN = Number.isNaN(value);
+  const isNegative = value <= 0;
+
+  return (isInfinite || isNegative || isNaN)
+    ? Left(createFiniteNonZeroPositiveNumberError('Value must be a finite, non-zero positive number'))
+    : Right(value as FiniteNonZeroPositiveNumber)
 };
