b34c62054faeac819f79aea2420302fa825726f4
SJB <stevenjburns@msn.com>
2025-03-15 02:42:30 -0500

    refactor: create index.ts files for domain primitives and values folders

---
+++ src/domain/values/index.ts
@@ -0,0 +1,7 @@
+export type { Angle } from './Angle/Angle.type.ts';
+export type { Mass } from './Mass/Mass.type.ts';
+export type { Temperature } from './Temperature/Temperature.value.ts';
+
+export { createAngle } from './Angle/Angle.factory.ts';
+export { createMass } from './Mass/Mass.factory.ts';
+export { createTemperature } from './Temperature/Temperature.factory.ts';
