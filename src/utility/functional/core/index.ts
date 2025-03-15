8f5464a25e6f246828b0a6ceabd57f274bfe115b
SJB <stevenjburns@msn.com>
2025-03-15 02:38:41 -0500

    refactor: add index.ts files to functional core and monads folders

---
+++ src/utility/functional/core/index.ts
@@ -0,0 +1,4 @@
+export { compose } from './compose/compose.ts';
+export { curry } from './curry/curry.ts';
+export { identity } from './identity/identity.ts';
+export { pipe } from './pipe/pipe.ts';
---
+++ src/utility/functional/monads/index.ts
@@ -0,0 +1,2 @@
+export type { Either} from './Either/Either.ts';
+export { Left, Right, mapEither, foldEither, chainEither } from './Either/Either.ts';
