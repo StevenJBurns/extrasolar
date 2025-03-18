da199eb5f9b7d0d117f2983fbd1fb6a7410c31f4
SJB <stevenjburns@msn.com>
2025-03-18 05:50:21 -0500

    test: syntax clean up in Angle factory

--- src/domain/values/Angle/Angle.factory.ts
+++ src/domain/values/Angle/Angle.factory.ts
@@ -7,9 +7,8 @@ import { type Either, Left, Right, chainEither } from '@utility/functional/monad
 export const createAngle = (value: number, unit: AngleUnit): Either<AngleError, Angle> =>
   chainEither<AngleError, void, Angle>(() => {
     const finiteResult = createFiniteNumber(value);
+
     return chainEither<AngleError, FiniteNumber, Angle>((finiteValue: FiniteNumber) =>
       Right(Object.freeze({ value: finiteValue, unit }))
-    )(
-      finiteResult.type === 'Left' ? Left(createAngleError('InvalidValue')) : finiteResult
-    );
+    )(finiteResult.type === 'Left' ? Left(createAngleError('InvalidValue')) : finiteResult);
   })(validateAngle(value, unit));
