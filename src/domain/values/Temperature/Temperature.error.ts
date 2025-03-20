6f6b35a1c00ad4ed233d76b78ebe569d03eb11da
SJB <stevenjburns@msn.com>
2025-03-19 22:19:12 -0500

    feat: re-work Temperature error, factory and validate to working state

--- src/domain/values/Temperature/Temperature.error.ts
+++ src/domain/values/Temperature/Temperature.error.ts
@@ -1,12 +1,11 @@
-import { DomainError } from "@domain/types";
-import { TemperatureErrorReason } from "@domain/values";
+import { TemperatureError, TemperatureErrorReason } from "@domain/values";
 
 const errorMessages: Record<TemperatureErrorReason, string> = {
   OutOfRange: 'Temperature value must be within range',
   InvalidUnit: "Temperature unit must be 'Kelvin' 'Celsius' or 'Fahrenheit'",
 };
 
-export const createTemperatureError = (reason: TemperatureErrorReason): DomainError => 
+export const createTemperatureError = (reason: TemperatureErrorReason): TemperatureError => 
   Object.freeze({
     code: 'TemperatureError',
     reason,
--- src/domain/values/Temperature/Temperature.validate.ts
+++ src/domain/values/Temperature/Temperature.validate.ts
@@ -1,16 +1,29 @@
-import { Temperature } from "@domain/values";
+import { TemperatureError, TemperatureScale } from '@domain/values';
+import { createTemperatureError } from '@domain/values/Temperature/Temperature.error';
+import { Either, Left, Right, chainEither } from '@utility/functional/monads';
 
-export function isTemperatureValueValid(temp: Temperature): boolean {
-  switch (temp.scale) {
-    case 'Kelvin':
-      return temp.value >= 0;
-    case 'Celsius':
-      return temp.value >= -273.15;
-    case 'Fahrenheit':
-      return temp.value >= -459.67;
-  };
+
+const TEMPERATURE_LOWER_BOUNDS: Record<TemperatureScale, number> = {
+  Kelvin: 0,
+  Celsius: -273.15,
+  Fahrenheit: -459.67,
 };
 
-export function isTemperatureScaleValid(temp: Temperature): boolean {
-  return ['Kelvin', 'Celsius', 'Fahrenheit'].includes(temp.scale);
+const checkValue = (value: number, scale: TemperatureScale): Either<TemperatureError, void> => {
+  return (value < TEMPERATURE_LOWER_BOUNDS[scale])
+    ? Left(createTemperatureError('OutOfRange'))
+    : Right(undefined);
 };
+
+const checkScale = (scale: string): Either<TemperatureError, TemperatureScale> => {
+  return ['Kelvin', 'Celsius', 'Fahrenheit'].includes(scale)
+    ? Right(scale as TemperatureScale)
+    : Left(createTemperatureError('InvalidUnit'))
+
+};
+
+export const validateTemperature = (value: number, scale: string): Either<TemperatureError, void> =>
+  chainEither((validScale: TemperatureScale) =>
+    checkValue(value, validScale))(
+      checkScale(scale)
+    );
