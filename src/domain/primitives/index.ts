81dfddbc0f72111f4bd6e9fb13511f2a6d388ec7
SJB <stevenjburns@msn.com>
2025-03-24 01:30:07 -0500

    chore: update primitives index.ts

--- src/domain/primitives/index.ts
+++ src/domain/primitives/index.ts
@@ -1,13 +1,9 @@
-export type {
-  NonEmptyString,
-  NonEmptyStringError,
-} from './NonEmptyString/NonEmptyString.types.ts';
+export type { NonEmptyString } from './NonEmptyString/NonEmptyString.types.ts';
 export type { FiniteNonZeroPositiveNumber } from './FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.type.ts';
-export type { FiniteNumber, FiniteNumberError } from './FiniteNumber/FiniteNumber.types.ts';
+export type { FiniteNumber } from './FiniteNumber/FiniteNumber.types.ts';
+export type { NormalizedValue } from './NormalizedValue/NormalizedValue.types.ts';
 
+export { createNonEmptyString } from './NonEmptyString/NonEmptyString.factory.ts';
 export { createFiniteNonZeroPositiveNumber } from './FiniteNonZeroPositiveNumber/FiniteNonZeroPositiveNumber.factory.ts';
 export { createFiniteNumber } from './FiniteNumber/FiniteNumber.factory.ts';
-export {
-  createNonEmptyString,
-  createNonEmptyStringError,
-} from './NonEmptyString/NonEmptyString.factory.ts';
+export { createNormalizedValue } from './NormalizedValue/NormalizedValue.factory.ts';
