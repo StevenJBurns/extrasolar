b2b18d6cb4e27570011337c5f0ec23ba5323ff89
SJB <stevenjburns@msn.com>
2025-03-27 04:41:35 -0500

    chore: placeholder factory file for PositiveNumber primitive

---
+++ src/domain/primitives/PositiveNumber/PositiveNumber.factory.ts
@@ -0,0 +1,5 @@
+// import type { PositiveNumber, PositiveNumberError } from './PositiveNumber.types.ts';
+
+export function createPositiveNumberError() {}
+
+export function createPositiveNumber() {}
