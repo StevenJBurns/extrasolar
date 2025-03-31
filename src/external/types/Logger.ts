0288b3e7a778ba74e1231803692de7cc9c97b994
SJB <stevenjburns@msn.com>
2025-03-31 12:28:51 -0500

    chore: move Logger types

--- src/external/types/Logger.ts
+++
@@ -1,5 +0,0 @@
-export type Logger = {
-  info: (message: string, context?: Record<string, unknown>) => void;
-  warn: (message: string, context?: Record<string, unknown>) => void;
-  error: (message: string, error?: unknown, context?: Record<string, unknown>) => void;
-};
