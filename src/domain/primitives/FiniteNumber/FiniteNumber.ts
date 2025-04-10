6ff937d34c40e4e38ff64127b3e46d9f2eb6b5ac
SJB <stevenjburns@msn.com>
2025-04-10 10:55:43 -0500

    refactor: overhaul FiniteNumber primitive to a cleaner single file

---
+++ src/domain/primitives/FiniteNumber/FiniteNumber.ts
@@ -0,0 +1,32 @@
+import { Brand, DomainError } from '@domain/types';
+import { Either, Left, Right } from '@utility/functional/monads';
+
+type FiniteNumber = Brand<'FiniteNumber', number>;
+type FiniteNumberError = DomainError<'FiniteNumberError'>;
+type ErrorReason = 'InvalidInput' | 'OutOfBounds';
+
+const errorMessageLookup: Record<ErrorReason, (context: unknown) => string> = {
+  OutOfBounds: context => `input value is infinite. received: ${context}`,
+  InvalidInput: context => `input value is invalid. received: ${context}`,
+};
+
+function createError(reason: ErrorReason, value: unknown) {
+  return Left({
+    type: 'FiniteNumberError',
+    reason,
+    message: errorMessageLookup[reason](value),
+  } as const);
+}
+
+function createFiniteNumber(inputValue: number): Either<FiniteNumberError, FiniteNumber> {
+  const reason: ErrorReason = isNaN(inputValue) ? 'InvalidInput' : 'OutOfBounds';
+
+  return Number.isFinite(inputValue)
+    ? Right(inputValue as FiniteNumber)
+    : createError(reason, inputValue);
+}
+
+export const FiniteNumber = {
+  create: createFiniteNumber,
+  isFinite: Number.isFinite,
+};
