0100ba002879c43055a998220c664ecf72f994e3
SJB <stevenjburns@msn.com>
2025-04-22 21:58:25 -0500

    refactor: update Star imports/exports

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,3 @@
-export { Star } from './entities/Star/Star.module.ts';
+export { Star } from './entities/Star/Star.ts';
 export { Planet } from './entities/Planet/Planet.module.ts';
 export { SolarSystem } from './entities/SolarSystem/SolarSystem.module.ts';
